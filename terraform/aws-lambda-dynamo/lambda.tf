68efcfa4c4f1697b47a69f8d1e75a72d4051de53
Kat Boddy <kboddy@semios.com>
2019-05-14 21:56:00 -0700

    terraform lambda base

---
+++ terraform/aws-lambda-dynamo/lambda.tf
@@ -0,0 +1,41 @@
+data "archive_file" "zip" {
+  type        = "zip"
+  source_file = "shortener.py"
+  output_path = "shortener.zip"
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    sid    = ""
+    effect = "Allow"
+
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type        = "Service"
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = "${data.aws_iam_policy_document.policy.json}"
+}
+
+resource "aws_lambda_function" "lambda" {
+  function_name = "shortener"
+
+  filename         = "${data.archive_file.zip.output_path}"
+  source_code_hash = "${data.archive_file.zip.output_base64sha256}"
+
+  role    = "${aws_iam_role.iam_for_lambda.arn}"
+  handler = "hello_lambda.lambda_handler"
+  runtime = "python3.6"
+
+  environment {
+    variables = {
+
+    }
+  }
+}
